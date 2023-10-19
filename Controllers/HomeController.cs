6e6846ccd37ae757f5d42be2bd9acac0b83cedb3
Johnatan Mendes <jonath2000.jm@gmail.com>
2023-10-19 20:52:28 -0300

    First Model-Controller-View - Department

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System.Diagnostics;
 
 namespace SalesWebMvc.Controllers
---
+++ Models/Department.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
